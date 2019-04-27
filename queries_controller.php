c0807ee31ebdf11d23af392e561b9489bab95184
Naufil865 <naufilmomincs@gmail.com>
2019-04-27 16:59:46 +0530

    mail by naufil

--- queries_controller.php
+++ queries_controller.php
@@ -8,4 +8,34 @@ $sql = "INSERT INTO queries (uname , email , question) VALUES ('".$name."','".$e
 if (mysqli_query($con, $sql)) {
     echo $sql;
 }
+
+
+require 'phpmailer/phpmailer.php' ;
+require 'phpmailer/Exception.php';
+require 'phpmailer/SMTP.php';
+
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+$subject = "Test Mail using PHP mailer";
+$content = "<b>This is a test mail using PHP mailer class.</b>";
+$mail->IsSMTP();
+$mail->SMTPDebug = 0;
+$mail->SMTPAuth = TRUE;
+$mail->SMTPSecure = "tls";
+$mail->Port     = 587;  
+$mail->Username = "nuamca786@gmail.com ";
+$mail->Password = "password";
+$mail->Host     = "smtp.gmail.com";
+$mail->Mailer   = "smtp";
+$mail->SetFrom("naufilmomincs@gmail.com", "Naufil");
+$mail->AddReplyTo("nuamca786@gmail.com ", "PHPPot");
+$mail->AddAddress("recipient address");
+$mail->Subject = $subject;
+$mail->WordWrap   = 80;
+$mail->MsgHTML($content);
+$mail->IsHTML(true);
+
+if(!$mail->Send()) 
+	echo "Problem on sending mail";
+else 
+echo "Mail sent";
 ?>
